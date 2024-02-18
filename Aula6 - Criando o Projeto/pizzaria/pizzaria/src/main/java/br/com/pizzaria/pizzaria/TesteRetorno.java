53db0035d7d9974841d4e4f38eac84ecd5dc0629
Reginaldo <regsena@gmail.com>
2024-02-17 20:35:16 -0400

    17/02/2024

---
+++ Aula6 - Criando o Projeto/pizzaria/pizzaria/src/main/java/br/com/pizzaria/pizzaria/TesteRetorno.java
@@ -0,0 +1,15 @@
+package br.com.pizzaria.pizzaria;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/teste")
+public class TesteRetorno {
+
+	@GetMapping
+	public String testeRetorno() {
+		return "Olá, este é o retorno do teste";
+	}
+}
